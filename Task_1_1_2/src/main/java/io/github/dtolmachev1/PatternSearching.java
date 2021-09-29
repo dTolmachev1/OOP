32f6da2bd46206b79a698d1e60011111ca81b96f
dTolmachev1 <d.tolmachev1@g.nsu.ru>
2021-09-29 13:22:43 +0700

    Task_1_1_2. Refactored algorithm and tests

--- Task_1_1_2/src/main/java/io/github/dtolmachev1/PatternSearching.java
+++ Task_1_1_2/src/main/java/io/github/dtolmachev1/PatternSearching.java
@@ -1,10 +1,16 @@
 package io.github.dtolmachev1;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,16 +20,16 @@ import java.util.List;
 public class PatternSearching {
     /**
      * <p>Finds all occurrences of a given pattern using z-function.</p>
-     * 
-     * @param fileName of a file for searching.
+     *
      * @param pattern desired substring.
+     * @param file for searching.
      * @return <code>List</code> with occurrences positions.
      */
-    public static List<Integer> findOccurrences(String fileName, String pattern) {
+    public static List<Integer> findOccurrences(String pattern, File file) {
         List<Integer> occurrences = new ArrayList<>();
         if(pattern.length() != 0) {
-            try (BufferedReader reader = Files.newBufferedReader(Paths.get(fileName), StandardCharsets.UTF_8)) {
-                occurrences = processText(reader, pattern);
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), CHARSET))) {
+                occurrences = findOccurrences(pattern, reader);
             }
             catch (IOException e) {
                 e.printStackTrace();
@@ -32,30 +38,68 @@ public class PatternSearching {
         return occurrences;
     }
 
-    /* finds all occurrences of a given pattern using precalculated z-array */
-    private static List<Integer> processText(BufferedReader reader, String pattern) {
+    /**
+     * <p>Finds all occurrences of a given pattern using z-function.</p>
+     * 
+     * @param pattern desired substring.
+     * @param path to the file for searching.
+     * @return <code>List</code> with occurrences positions.
+     */
+    public static List<Integer> findOccurrences(String pattern, Path path) {
         List<Integer> occurrences = new ArrayList<>();
-        char[] buffer = new char[pattern.length()];
+        if(pattern.length() != 0) {
+            try (BufferedReader reader = Files.newBufferedReader(path, CHARSET)) {
+                occurrences = findOccurrences(pattern, reader);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return occurrences;
+    }
+
+    /**
+     * <p>Finds all occurrences of a given pattern using z-function.</p>
+     *
+     * @param pattern desired substring.
+     * @param text string for searching.
+     * @return <code>List</code> with occurrences positions.
+     */
+    public static List<Integer> findOccurrences(String pattern, String text) {
+        List<Integer> occurrences = new ArrayList<>();
+        if(pattern.length() != 0) {
+            try (BufferedReader reader = new BufferedReader(new StringReader(text))) {
+                occurrences = findOccurrences(pattern, reader);
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return occurrences;
+    }
+
+    /**
+     * <p>Finds all occurrences of a given pattern using z-function.</p>
+     *
+     * @param pattern desired substring.
+     * @param reader with stream for  searching.
+     * @return <code>List</code> with occurrences positions.
+     */
+    public static List<Integer> findOccurrences(String pattern, Reader reader) {
+        List<Integer> occurrences = new ArrayList<>();
         try {
-            reader.read(buffer, 0, buffer.length - 1);
-            int[] zArr = zFunction(pattern);
-            int left = 0;  // left z-box boundary
-            int right = 0;  // right z-box boundary
-            for (int i = 0; reader.read(buffer, buffer.length - 1, 1) != -1; i++) {
-                if (((i == 0) || (i > right)) || ((i != 0) && (i <= right) && (zArr[i - left] >= right - i + 1))) {
-                    left = i;
-                    if ((i == 0) || (i > right)) {
-                        right = i;
+            char[] buffer = new char[16*pattern.length()];
+            pattern.getChars(0, pattern.length(), buffer, 0);
+            buffer[pattern.length()] = DELIMITER;
+            reader.read(buffer, pattern.length() + 1, pattern.length() - 1);
+            for(int i = 0; reader.read(buffer, 2 * pattern.length(), buffer.length - 2 * pattern.length()) != -1; i++) {
+                int[] zArr = zFunction(buffer);
+                for(int j = pattern.length() + 1; j < buffer.length; j++) {
+                    if(zArr[j] == pattern.length()) {
+                        occurrences.add(i * (buffer.length - 2 * pattern.length()) + j - pattern.length() - 1);
                     }
-                    while ((right - i < buffer.length) && (right - left < buffer.length) && (buffer[right-i] == pattern.charAt(right - left))) {
-                        right++;
-                    }
-                    if (right - left == buffer.length) {
-                        occurrences.add(left);
-                    }
-                    right--;
                 }
-                System.arraycopy(buffer, 1, buffer, 0, buffer.length - 1);
+                System.arraycopy(buffer, buffer.length - pattern.length() + 1, buffer, pattern.length() + 1, pattern.length() - 1);
             }
         }
         catch(IOException e) {
@@ -64,25 +108,24 @@ public class PatternSearching {
         return occurrences;
     }
 
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
+    private static final char DELIMITER = '\uFFFD';
+
     /* builds z-array for a given string */
-    private static int[] zFunction(String string) {
-        int[] zArr = new int[string.length()];  // for storing z-array
+    private static int[] zFunction(char[] block) {
+        int[] zArr = new int[block.length];  // for storing z-array
         int left = 0;  // left z-box boundary
         int right = 0;  // right z-box boundary
-        for(int i = 1; i < string.length(); i++) {
-            if((i > right) || ((i <= right) && (zArr[i-left] >= right - i + 1))) {
-                left = i;
-                if(i > right) {
-                    right = i;
-                }
-                while((right < string.length()) && (string.charAt(right) == string.charAt(right - left))) {
-                    right++;
-                }
-                zArr[i] = right - left;
-                right--;
+        for(int i = 1; i < block.length; i++) {
+            if(i <= right) {
+                zArr[i] = Math.min(right - i + 1, zArr[i-left]);
             }
-            else if(zArr[i-left] < right - i + 1) {
-                zArr[i] = zArr[i-left];
+            while((i + zArr[i] < block.length) && (block[zArr[i]] == block[i+zArr[i]])) {
+                zArr[i]++;
+            }
+            if(i + zArr[i] - 1 > right) {
+                left = i;
+                right = i + zArr[i] - 1;
             }
         }
         return zArr;
