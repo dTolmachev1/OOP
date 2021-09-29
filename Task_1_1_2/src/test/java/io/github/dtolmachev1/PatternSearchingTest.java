759f2b2d809fb02d15aa277d28957496d2d3dd23
dTolmachev1 <d.tolmachev1@g.nsu.ru>
2021-09-29 13:22:43 +0700

    Task_1_1_2. Refactored algorithm and tests

--- Task_1_1_2/src/test/java/io/github/dtolmachev1/PatternSearchingTest.java
+++ Task_1_1_2/src/test/java/io/github/dtolmachev1/PatternSearchingTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import java.io.BufferedWriter;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
@@ -35,18 +36,18 @@ class PatternSearchingTest {
 
         /* creating files */
         try {  // empty file
-            if (!Files.exists(EMPTY_FILE)) {
-                Files.createFile(EMPTY_FILE);
+            if (!Files.exists(EMPTY_PATH)) {
+                Files.createFile(EMPTY_PATH);
             }
         }
         catch(IOException e) {
             e.printStackTrace();
         }
         try {  // single-character file
-            if(!Files.exists(SINGLE_CHARACTER_FILE)) {
-                Files.createFile(SINGLE_CHARACTER_FILE);
+            if(!Files.exists(SINGLE_CHARACTER_PATH)) {
+                Files.createFile(SINGLE_CHARACTER_PATH);
             }
-            BufferedWriter writer = Files.newBufferedWriter(SINGLE_CHARACTER_FILE, CHARSET);
+            BufferedWriter writer = Files.newBufferedWriter(SINGLE_CHARACTER_PATH, CHARSET);
             writer.write(singleCharacter);
             writer.close();
         }
@@ -54,10 +55,10 @@ class PatternSearchingTest {
             e.printStackTrace();
         }
         try {  // small file
-            if(!Files.exists(SMALL_FILE)) {
-                Files.createFile(SMALL_FILE);
+            if(!Files.exists(SMALL_PATH)) {
+                Files.createFile(SMALL_PATH);
             }
-            BufferedWriter writer = Files.newBufferedWriter(SMALL_FILE, CHARSET);
+            BufferedWriter writer = Files.newBufferedWriter(SMALL_PATH, CHARSET);
             writer.write(small);
             writer.close();
         }
@@ -65,10 +66,10 @@ class PatternSearchingTest {
             e.printStackTrace();
         }
         try {  // medium file
-            if(!Files.exists(MEDIUM_FILE)) {
-                Files.createFile(MEDIUM_FILE);
+            if(!Files.exists(MEDIUM_PATH)) {
+                Files.createFile(MEDIUM_PATH);
             }
-            BufferedWriter writer = Files.newBufferedWriter(MEDIUM_FILE, CHARSET);
+            BufferedWriter writer = Files.newBufferedWriter(MEDIUM_PATH, CHARSET);
             writer.write(medium);
             writer.close();
         }
@@ -81,25 +82,25 @@ class PatternSearchingTest {
     static void tearDown() {
         /* deleting files */
         try {  // empty file
-            Files.deleteIfExists(EMPTY_FILE);
+            Files.deleteIfExists(EMPTY_PATH);
         }
         catch(IOException e) {
             e.printStackTrace();
         }
         try {  // single-character file
-            Files.deleteIfExists(SINGLE_CHARACTER_FILE);
+            Files.deleteIfExists(SINGLE_CHARACTER_PATH);
         }
         catch(IOException e) {
             e.printStackTrace();
         }
         try {  // small file
-            Files.deleteIfExists(SMALL_FILE);
+            Files.deleteIfExists(SMALL_PATH);
         }
         catch(IOException e) {
             e.printStackTrace();
         }
         try {  // medium file
-            Files.deleteIfExists(MEDIUM_FILE);
+            Files.deleteIfExists(MEDIUM_PATH);
         }
         catch(IOException e) {
             e.printStackTrace();
@@ -110,8 +111,12 @@ class PatternSearchingTest {
     @DisplayName("Empty pattern, Empty text, Text doesn't contain this pattern")
     void findOccurrences_EmptyEmptyNotContain() {
         String pattern = "";
-        src = PatternSearching.findOccurrences(EMPTY_FILE.toString(), pattern);
         exp = findOccurrences(pattern, empty);
+        src = PatternSearching.findOccurrences(pattern, EMPTY_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, EMPTY_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, empty);
         Assertions.assertEquals(src, exp);
     }
 
@@ -119,8 +124,12 @@ class PatternSearchingTest {
     @DisplayName("Single-character pattern, Empty text, Text doesn't contain this pattern")
     void findOccurrences_SingleCharacterEmptyNotContain() {
         String pattern = generateRandomString(1);
-        src = PatternSearching.findOccurrences(EMPTY_FILE.toString(), pattern);
-        exp = findOccurrences(singleCharacter, pattern);
+        exp = findOccurrences(pattern, empty);
+        src = PatternSearching.findOccurrences(pattern, EMPTY_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, EMPTY_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, empty);
         Assertions.assertEquals(src, exp);
     }
 
@@ -128,8 +137,12 @@ class PatternSearchingTest {
     @DisplayName("Empty pattern, Single-character text, Text doesn't contain this pattern")
     void findOccurrences_EmptySingleCharacterNotContain() {
         String pattern = "";
-        src = PatternSearching.findOccurrences(SINGLE_CHARACTER_FILE.toString(), pattern);
         exp = findOccurrences(pattern, singleCharacter);
+        src = PatternSearching.findOccurrences(pattern, SINGLE_CHARACTER_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SINGLE_CHARACTER_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, singleCharacter);
         Assertions.assertEquals(src, exp);
     }
 
@@ -140,8 +153,12 @@ class PatternSearchingTest {
         while(pattern.equals(singleCharacter)) {
             pattern = generateRandomString(singleCharacter.length());
         }
-        src = PatternSearching.findOccurrences(SINGLE_CHARACTER_FILE.toString(), pattern);
         exp = findOccurrences(pattern, singleCharacter);
+        src = PatternSearching.findOccurrences(pattern, SINGLE_CHARACTER_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SINGLE_CHARACTER_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, singleCharacter);
         Assertions.assertEquals(src, exp);
     }
 
@@ -149,8 +166,12 @@ class PatternSearchingTest {
     @DisplayName("Single-character pattern, Single-character text, text contains this pattern")
     void findOccurrences_SingleCharacterSingleCharacterContains() {
         String pattern = singleCharacter;
-        src = PatternSearching.findOccurrences(SINGLE_CHARACTER_FILE.toString(), pattern);
         exp = findOccurrences(pattern, singleCharacter);
+        src = PatternSearching.findOccurrences(pattern, SINGLE_CHARACTER_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SINGLE_CHARACTER_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, singleCharacter);
         Assertions.assertEquals(src, exp);
     }
 
@@ -158,8 +179,12 @@ class PatternSearchingTest {
     @DisplayName("Multi-character pattern, Single-character text, Text doesn't contain this pattern")
     void findOccurrences_MultiCharacterSingleCharacterNotContain() {
         String pattern = generateRandomString(2 * singleCharacter.length());
-        src = PatternSearching.findOccurrences(SINGLE_CHARACTER_FILE.toString(), pattern);
         exp = findOccurrences(pattern, singleCharacter);
+        src = PatternSearching.findOccurrences(pattern, SINGLE_CHARACTER_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SINGLE_CHARACTER_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, singleCharacter);
         Assertions.assertEquals(src, exp);
     }
 
@@ -167,8 +192,12 @@ class PatternSearchingTest {
     @DisplayName("Empty pattern, Small text, Text doesn't contain this pattern")
     void findOccurrences_EmptySmallNotContain() {
         String pattern = "";
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -176,8 +205,12 @@ class PatternSearchingTest {
     @DisplayName("Single-character pattern, Small text")
     void findOccurrences_SingleCharacterSmall() {
         String pattern = generateRandomString(1);
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -186,8 +219,12 @@ class PatternSearchingTest {
     void findOccurrences_SingleCharacterSmallContains() {
         int position = random.nextInt(small.length());
         String pattern = small.substring(position, position + 1);
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -196,8 +233,12 @@ class PatternSearchingTest {
     void findOccurrences_MultiCharacterSmallStarts() {
         int position = 1 + random.nextInt(small.length());
         String pattern = small.substring(0, position);
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -206,8 +247,12 @@ class PatternSearchingTest {
     void findOccurrences_MultiCharacterSmallEnds() {
         int position = random.nextInt(small.length());
         String pattern = small.substring(position);
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -215,8 +260,12 @@ class PatternSearchingTest {
     @DisplayName("Multi-character pattern, Small text")
     void findOccurrences_MultiCharacterSmall() {
         String pattern = generateRandomString(1 + random.nextInt(small.length()));
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -225,8 +274,12 @@ class PatternSearchingTest {
     void findOccurrences_MultiCharacterSmallContains() {
         int position = random.nextInt(small.length());
         String pattern = small.substring(position, position + random.nextInt(small.length() - position + 1));
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -237,8 +290,12 @@ class PatternSearchingTest {
         while(pattern.equals(small)) {
             small = generateRandomString(small.length());
         }
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -246,8 +303,12 @@ class PatternSearchingTest {
     @DisplayName("Small pattern, Small text, Text contains this pattern")
     void findOccurrences_SmallSmallContains() {
         String pattern = small;
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -255,8 +316,12 @@ class PatternSearchingTest {
     @DisplayName("Multi-character pattern,  text, Text doesn't contain this pattern")
     void findOccurrences_MultiCharacterSmallNotContain() {
         String pattern = generateRandomString(2 * small.length());
-        src = PatternSearching.findOccurrences(SMALL_FILE.toString(), pattern);
         exp = findOccurrences(pattern, small);
+        src = PatternSearching.findOccurrences(pattern, SMALL_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, SMALL_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, small);
         Assertions.assertEquals(src, exp);
     }
 
@@ -264,8 +329,12 @@ class PatternSearchingTest {
     @DisplayName("Multi-character pattern, Medium text")
     void findOccurrences_MultiCharacterMedium() {
         String pattern = generateRandomString(2048);
-        src = PatternSearching.findOccurrences(MEDIUM_FILE.toString(), pattern);
         exp = findOccurrences(pattern, medium);
+        src = PatternSearching.findOccurrences(pattern, MEDIUM_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, MEDIUM_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, medium);
         Assertions.assertEquals(src, exp);
     }
 
@@ -274,8 +343,12 @@ class PatternSearchingTest {
     void findOccurrences_MultiCharacterMediumContains() {
         int position = random.nextInt(medium.length() - 2048);
         String pattern = medium.substring(position, position + 2048);
-        src = PatternSearching.findOccurrences(MEDIUM_FILE.toString(), pattern);
         exp = findOccurrences(pattern, medium);
+        src = PatternSearching.findOccurrences(pattern, MEDIUM_FILE);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, MEDIUM_PATH);
+        Assertions.assertEquals(src, exp);
+        src = PatternSearching.findOccurrences(pattern, medium);
         Assertions.assertEquals(src, exp);
     }
 
@@ -287,10 +360,15 @@ class PatternSearchingTest {
     private static String small;
     private static String medium;
     private static final Charset CHARSET = StandardCharsets.UTF_8;
-    private static final Path EMPTY_FILE = Paths.get("src", "test", "resources", "empty.txt");
-    private static final Path SINGLE_CHARACTER_FILE = Paths.get("src", "test", "resources", "single-character.txt");
-    private static final Path SMALL_FILE = Paths.get("src", "test", "resources", "small.txt");
-    private static final Path MEDIUM_FILE = Paths.get("src", "test", "resources", "medium.txt");
+    private static final char DELIMITER = '\uFFFD';
+    private static final File EMPTY_FILE = new File("src" + File.separator + "test" + File.separator + "resources" + File.separator + "empty.txt");
+    private static final File SINGLE_CHARACTER_FILE = new File("src" + File.separator + "test" + File.separator + "resources" + File.separator + "single-character.txt");
+    private static final File SMALL_FILE = new File("src" + File.separator + "test" + File.separator + "resources" + File.separator + "small.txt");
+    private static final File MEDIUM_FILE = new File("src" + File.separator + "test" + File.separator + "resources" + File.separator + "medium.txt");
+    private static final Path EMPTY_PATH = Paths.get("src", "test", "resources", "empty.txt");
+    private static final Path SINGLE_CHARACTER_PATH = Paths.get("src", "test", "resources", "single-character.txt");
+    private static final Path SMALL_PATH = Paths.get("src", "test", "resources", "small.txt");
+    private static final Path MEDIUM_PATH = Paths.get("src", "test", "resources", "medium.txt");
 
     /* reference function to find all occurrences of a given pattern */
     private List<Integer> findOccurrences(String pattern, String text) {
@@ -310,6 +388,6 @@ class PatternSearchingTest {
 
     /* generates random string of a specific length */
     private static String generateRandomString(int length) {
-        return random.ints(Character.MIN_VALUE, Character.MAX_VALUE+1).filter(codePoint -> !Character.isSurrogate((char) codePoint)).limit(length).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
+        return random.ints(Character.MIN_VALUE, Character.MAX_VALUE+1).filter(codePoint -> (!Character.isSurrogate((char) codePoint)) && (((char) codePoint) != DELIMITER)).limit(length).collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString();
     }
 }
