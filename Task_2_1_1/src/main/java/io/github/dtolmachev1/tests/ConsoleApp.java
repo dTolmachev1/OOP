c1d7eb3cdde5733dba7b6641c3ff051d0bd84831
dtolmachev1 <d.tolmachev1@g.nsu.ru>
2022-02-21 17:02:23 +0700

    Task_2_1_1. Bug fixes and performance improvements

--- Task_2_1_1/src/main/java/io/github/dtolmachev1/tests/ConsoleApp.java
+++ Task_2_1_1/src/main/java/io/github/dtolmachev1/tests/ConsoleApp.java
@@ -7,11 +7,20 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class ConsoleApp {
+    private static Random random;
+    private static Primes sequentialPrimes;  // for sequential execution
+    private static Primes streamPrimes;  // for execution using parallel stream
+    private static Primes concurrentPrimes;  // for execution with multithreaded
+    private static ExecutionTime executionTime;  // for time measuring
+    private static final int PRIME = 1048571;  // prime number
+    private static final int SEMIPRIME = 1048561;  // semiprime number
+    private static final int N = 100000;  // numbers in the array
+
     public static void main(String[] args) {
         random = new Random();
         int[] numbers = new int[N];
-        Arrays.fill(numbers, 1000007);
-        numbers[Random.nextInt(N - 1)] = 1014049;
+        Arrays.fill(numbers, PRIME);
+        numbers[random.nextInt(N - 1)] = SEMIPRIME;
         sequentialPrimes = new SequentialPrimes(numbers);
         streamPrimes = new StreamPrimes(numbers);
         concurrentPrimes = new ConcurrentPrimes(numbers);
@@ -20,11 +29,4 @@ public class ConsoleApp {
         System.out.println("Execution using parallel stream: " + executionTime.measure(streamPrimes::areAllPrimes) + ".");
         System.out.println("Execution with multithreaded: " + executionTime.measure(concurrentPrimes::areAllPrimes) + ".");
     }
-
-    private static Random random;
-    private static Primes sequentialPrimes;  // for sequential execution
-    private static Primes streamPrimes;  // for execution using parallel stream
-    private static Primes concurrentPrimes;  // for execution with multithreaded
-    private static ExecutionTime executionTime;  // for time measuring
-    private static final int N = 10000000;  // numbers in the array
 }
